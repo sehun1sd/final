4d8a05a505679ecbf7898b6f4b8ed0a64a1c43f6
sehun1sd <sehanalfarisi9@gmail.com>
2025-05-30 21:11:55 +0700

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, X, Mic } from 'lucide-react';
 import LanguageToggle from './LanguageToggle';
-import GooeyNav from './SettingsNav';
+import GooeyNav from './SettingNav';
 import type { Language } from '../constants/translations';
 
 interface NavbarProps {
